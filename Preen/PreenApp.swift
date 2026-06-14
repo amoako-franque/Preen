e9266f5c880b34271462b463a91e58a64e987181
amoako-franque <frank.amoako-dev@outlook.com>
2026-06-14 23:06:33 +0000

    Initial Commit

---
+++ Preen/PreenApp.swift
@@ -0,0 +1,32 @@
+//
+//  PreenApp.swift
+//  Preen
+//
+//  Created by franque on 14/06/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PreenApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ PreenTests/PreenTests.swift
@@ -0,0 +1,17 @@
+//
+//  PreenTests.swift
+//  PreenTests
+//
+//  Created by franque on 14/06/2026.
+//
+
+import Testing
+@testable import Preen
+
+struct PreenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
